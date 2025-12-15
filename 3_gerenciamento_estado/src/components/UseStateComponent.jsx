e97bf36eca9107061e097f7d118ec582eb2741c1
Algoritmo2018 <luischilembomateus@gmail.com>
2025-12-15 15:58:45 +0100

    gerenciamento de estado

--- 3_gerenciamento_estado/src/components/UseStateComponent.jsx
+++ 3_gerenciamento_estado/src/components/UseStateComponent.jsx
@@ -1,19 +1,40 @@
 import {useState} from 'react'
 
 // useState é um hook
+
+//sempre que vamos  alterar o valor de algo => useState
 const UseStateComponent = () => {
     // variavel de consulta, e uma de alteração, inicio o hook
     const [count,setCount]= useState(0);
    
     const increment=()=>{
-        setCount(count+1);
+        setCount((prevCount)=>prevCount+1);
+       // setCount(count+1);
         console.log(count);
+    };
+    const [user,setUser]= useState({
+name:"Ana",
+age:25,
+hobbies:["Leitura", "Programação"]
+    })
+    const updateUserAge=()=> {
+      setUser((prevUser)=>(
+{
+  ...prevUser,
+  age: prevUser.age+1,
+}
+      ))
     }
   return (
     <div>
         <h2>contador</h2>
         <p>Você clicou {count} vezes</p>
         <button onClick={increment}>Incrementar</button>
+
+        <p>
+          Nome: {user.name}e idade: {user.age}
+        </p>
+        <button onClick={updateUserAge}>incrementar Idade</button>
     </div>
   )
 }
