47f6c7bbca5c4171c7d1629671fc68f18cb9a014
Algoritmo2018 <luischilembomateus@gmail.com>
2025-12-15 11:19:50 +0100

    componentes

--- 1_componentes/src/App.jsx
+++ 1_componentes/src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//componente pai
+import { useState } from 'react' 
+
+//importando componente filho
+import FunctionalComponent from './components/FunctionalComponent'
+import ClassComponent from './components/ClassComponent'
+import PropsExample from './components/PropsExample'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App() { 
   return (
     <>
       <h1>Hello world</h1>
+      {/** uiliazando o componente importado no JSX */}
+      <FunctionalComponent/> 
+      <ClassComponent/>
+      <PropsExample nome="Mateus" idade={22}/> 
     </>
   )
 }
---
+++ 1_componentes/src/components/FunctionalComponent.jsx
@@ -0,0 +1,8 @@
+//componente filho
+function FunctionalComponent() {
+    return (
+        <h2>Testando componente react</h2>
+    )
+}
+
+export default FunctionalComponent;
