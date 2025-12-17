f17e6411d205464964123a8997a930909beaad08
Algoritmo2018 <luischilembomateus@gmail.com>
2025-12-17 14:49:42 +0100

    consumindo api com o react

--- 8_consumindo_api_com_react/movies_lib/src/App.jsx
+++ 8_consumindo_api_com_react/movies_lib/src/App.jsx
@@ -1,13 +1,13 @@
+import {  Outlet } from 'react-router-dom'
+import Navbar from "./components/Navbar"
 import './App.css'
 
 function App() {
   return (
     <>
       <div className="App">
-        <nav id="navbar">
-          
-        </nav>
-        <h2>Movies lib</h2>
+        <Navbar/> 
+        <Outlet/>
       </div>
     </>
   )
